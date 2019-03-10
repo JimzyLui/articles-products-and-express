f1af26316b524cdac14513ae458f58019647149c
Jimzy <JimzyLui@users.noreply.github.com>
2019-03-10 02:42:10 -1000

    tweaked console msg

--- routes/articles.js
+++ routes/articles.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const DS_Articles = require("../db/DS_Articles");
 
 router.use("/", (req, res, next) => {
-  console.log("req.body: ", req.body);
+  // console.log("req.body: ", req.body);
   next();
 });
 
